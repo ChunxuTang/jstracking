a61ea641ac541a99c5137526fa1ad8a5403287d6
JoshuaTang <1240604020@qq.com>
2017-07-07 20:53:32 +0800

    simplify scaling function

--- build/tracking.js
+++ build/tracking.js
@@ -2261,59 +2261,23 @@ Scale.scale = 1.0;
  */
 Scale.adjustScale = function (width, height) {
   var PIXEL_THRESHOLD = 50000;
-  // this.scale =
-  //   this.normalizeScale(1.0 / (Math.sqrt(width * height / PIXEL_THRESHOLD)));
-  // console.warn('scale computed', this.scale);
   var ratio = 1 / Math.sqrt(width * height / PIXEL_THRESHOLD);
-  ratio = ratio > 1 ? 1 : ratio;
-  this.scale = Math.round(ratio * 10) / 10;
+  this.scale = this.normalizeScale(ratio);
   console.warn('scale computed', this.scale);
 };
 
 /**
  * Normalizes the raw scale to avoid rounding issues.
- * Note: take from http://jsfiddle.net/gamealchemist/r6aVp.
  * @param {number} s Raw scale.
  * @returns {number} Normalized scale.
  * @static
  */
 Scale.normalizeScale = function (s) {
-  if (s > 1) {
-    throw 's must be <1';
-  }
-  s = 0 | 1 / s;
-  var l = this.log2(s);
-  var mask = 1 << l;
-  var accuracy = 4;
-  while (accuracy && l) {
-    l--;
-    mask |= 1 << l;
-    accuracy--;
+  if (s >= 1) {
+    return 1;
   }
 
-  return 1 / (s & mask);
-};
-
-/**
- * Finds the log base 2 of an N-bit integer.
- * Note: taken from http://graphics.stanford.edu/~seander/bithacks.html.
- * @param v N-bit number.
- * @returns {number} Log base 2 of the number.
- * @static
- */
-Scale.log2 = function (v) {
-  var b = [0x2, 0xC, 0xF0, 0xFF00, 0xFFFF0000];
-  var S = [1, 2, 4, 8, 16];
-  var i = 0,
-      r = 0;
-
-  for (i = 4; i >= 0; i--) {
-    if (v & b[i]) {
-      v >>= S[i];
-      r |= S[i];
-    }
-  }
-  return r;
+  return Math.round(s * 10) / 10;
 };
 
 module.exports = Scale;
--- src/utils/Scale.js
+++ src/utils/Scale.js
@@ -21,58 +21,23 @@ Scale.scale = 1.0;
  */
 Scale.adjustScale = function (width, height) {
   const PIXEL_THRESHOLD = 50000;
-  // this.scale =
-  //   this.normalizeScale(1.0 / (Math.sqrt(width * height / PIXEL_THRESHOLD)));
-  // console.warn('scale computed', this.scale);
   let ratio = 1 / (Math.sqrt(width * height / PIXEL_THRESHOLD));
-  ratio = ratio > 1 ? 1 : ratio;
-  this.scale = Math.round(ratio * 10) / 10;
+  this.scale = this.normalizeScale(ratio);
   console.warn('scale computed', this.scale);
 };
 
 /**
  * Normalizes the raw scale to avoid rounding issues.
- * Note: take from http://jsfiddle.net/gamealchemist/r6aVp.
  * @param {number} s Raw scale.
  * @returns {number} Normalized scale.
  * @static
  */
 Scale.normalizeScale = function (s) {
-  if (s > 1) {
-    throw('s must be <1');
-  }
-  s = 0 | (1 / s);
-  let l = this.log2(s);
-  let mask = 1 << l;
-  let accuracy = 4;
-  while (accuracy && l) {
-    l--;
-    mask |= 1 << l;
-    accuracy--;
+  if (s >= 1) {
+    return 1;
   }
 
-  return 1 / ( s & mask );
-};
-
-/**
- * Finds the log base 2 of an N-bit integer.
- * Note: taken from http://graphics.stanford.edu/~seander/bithacks.html.
- * @param v N-bit number.
- * @returns {number} Log base 2 of the number.
- * @static
- */
-Scale.log2 = function (v) {
-  const b = [0x2, 0xC, 0xF0, 0xFF00, 0xFFFF0000];
-  const S = [1, 2, 4, 8, 16];
-  let i = 0, r = 0;
-
-  for (i = 4; i >= 0; i--) {
-    if (v & b[i]) {
-      v >>= S[i];
-      r |= S[i];
-    }
-  }
-  return r;
+  return Math.round(s * 10) / 10;
 };
 
 module.exports = Scale;
